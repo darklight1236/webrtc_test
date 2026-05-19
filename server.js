2f7c53257fcc572f411249c3521d23751c975645
darklight1236 <gamasutora@yandex.ru>
2026-05-19 09:59:59 +0400

    next 7

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const { Server } = require("socket.io");
 
 const app = express();
 
-app.use(express.static(__dirname));
-
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
+
+app.use(express.static(__dirname));
 
 const server = http.createServer(app);
 
@@ -24,23 +24,26 @@ io.on("connection", socket => {
     console.log("CONNECTED:", socket.id);
 
     socket.on("offer", offer => {
+        console.log("OFFER RECEIVED");
         socket.broadcast.emit("offer", offer);
     });
 
     socket.on("answer", answer => {
+        console.log("ANSWER RECEIVED");
         socket.broadcast.emit("answer", answer);
     });
 
     socket.on("candidate", candidate => {
+        console.log("ICE CANDIDATE");
         socket.broadcast.emit("candidate", candidate);
     });
 
     socket.on("disconnect", () => {
-        console.log("DISCONNECTED");
+        console.log("DISCONNECTED:", socket.id);
     });
 
 });
 
 server.listen(3000, () => {
-    console.log("SERVER STARTED");
+    console.log("SERVER STARTED ON PORT 3000");
 });
