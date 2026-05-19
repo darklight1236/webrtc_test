fefe22b884f62bd8ec6479c3c7f820b23c41b924
darklight1236 <gamasutora@yandex.ru>
2026-05-19 09:20:27 +0400

    first commit

---
+++ client/app.js
@@ -0,0 +1,73 @@
+(() => {
+
+    const localVideo = document.getElementById("local");
+    const remoteVideo = document.getElementById("remote");
+
+    const socket = io("http://localhost:3000");
+
+    const peer = new RTCPeerConnection({
+        iceServers: [
+            {
+                urls: "stun:stun.l.google.com:19302"
+            }
+        ]
+    });
+
+    async function start() {
+
+        const stream = await navigator.mediaDevices.getUserMedia({
+            video: true,
+            audio: true
+        });
+
+        localVideo.srcObject = stream;
+
+        stream.getTracks().forEach(track => {
+            peer.addTrack(track, stream);
+        });
+
+    }
+
+    peer.ontrack = event => {
+        remoteVideo.srcObject = event.streams[0];
+    };
+
+    peer.onicecandidate = event => {
+        if (event.candidate) {
+            socket.emit("candidate", event.candidate);
+        }
+    };
+
+    socket.on("candidate", async candidate => {
+        await peer.addIceCandidate(candidate);
+    });
+
+    socket.on("offer", async offer => {
+
+        await peer.setRemoteDescription(offer);
+
+        const answer = await peer.createAnswer();
+
+        await peer.setLocalDescription(answer);
+
+        socket.emit("answer", answer);
+
+    });
+
+    socket.on("answer", async answer => {
+        await peer.setRemoteDescription(answer);
+    });
+
+    start();
+
+    window.call = async () => {
+
+        const offer = await peer.createOffer();
+
+        await peer.setLocalDescription(offer);
+
+        socket.emit("offer", offer);
+
+    };
+
+})();
---
+++ server/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+});
+
+io.on("connection", socket => {
+
+    console.log("User connected:", socket.id);
+
+    socket.on("offer", offer => {
+        socket.broadcast.emit("offer", offer);
+    });
+
+    socket.on("answer", answer => {
+        socket.broadcast.emit("answer", answer);
+    });
+
+    socket.on("candidate", candidate => {
+        socket.broadcast.emit("candidate", candidate);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+    });
+
+});
+
+server.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
