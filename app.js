be194d27d8f64105cbf81897d8b7d8b316774f47
darklight1236 <gamasutora@yandex.ru>
2026-05-21 14:45:12 +0400

    add fix user left from channel 3

--- app.js
+++ app.js
@@ -139,10 +139,42 @@
 
 
     // ДЕНДЖЕРОС!!!! БИ КАРЕФУЛ!!!
+    // function cleanupPeer(id) {
+
+    //     if (peers[id]) {
+    //         peers[id].close();
+    //         delete peers[id];
+    //     }
+
+    //     if (pendingCandidates[id]) {
+    //         delete pendingCandidates[id];
+    //     }
+
+    //     const client = document.getElementById(`client-${id}`);
+
+    //     if (client) {
+
+    //         leaveSound.currentTime = 0;
+    //         leaveSound.play().catch(() => { });
+
+    //         client.remove();
+    //     }
+
+    //     console.log("CLEANED UP:", id);
+    // }
+
     function cleanupPeer(id) {
 
-        if (peers[id]) {
-            peers[id].close();
+        const pc = peers[id];
+
+        if (pc) {
+
+            pc.ontrack = null;
+            pc.onicecandidate = null;
+            pc.onconnectionstatechange = null;
+
+            pc.close();
+
             delete peers[id];
         }
 
@@ -150,9 +182,24 @@
             delete pendingCandidates[id];
         }
 
-        const client = document.getElementById(`client-${id}`);
+        const client =
+            document.getElementById(`client-${id}`);
 
         if (client) {
+
+            const video =
+                client.querySelector("video");
+
+            if (video) {
+
+                if (video.srcObject) {
+
+                    video.srcObject = null;
+                }
+
+                video.removeAttribute("src");
+                video.load();
+            }
 
             leaveSound.currentTime = 0;
             leaveSound.play().catch(() => { });
@@ -160,11 +207,9 @@
             client.remove();
         }
 
-        console.log("CLEANED UP:", id);
+        console.log("CLEANED:", id);
     }
 
-
-    
 
     // ───────────────────────────────
     // USERS
