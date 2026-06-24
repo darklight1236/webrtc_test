15ba20a3edad7e7c12d718841e40d770a8240c30
darklight1236 <gamasutora@yandex.ru>
2026-06-25 01:41:06 +0400

    try to fix site 1

--- app.js
+++ app.js
@@ -40,7 +40,7 @@
             iceServers: [
                 { urls: "stun:stun.l.google.com:19302" },
                 {
-                    urls: "turn:82.26.150.172:3478",
+                    urls: "turn:159.194.214.116:3478",
                     username: "test",
                     credential: "test123"
                 }
